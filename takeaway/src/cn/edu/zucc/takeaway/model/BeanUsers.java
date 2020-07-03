ea5459722a8a8225a12800acec816afc95e69e93
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:58:19 +0800

    Create BeanUsers.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanUsers.java
@@ -0,0 +1,51 @@
+package cn.edu.zucc.takeaway.model;
+
+import java.util.Date;
+
+public class BeanUsers {
+	private int shop_no;
+	private String shop_name;
+	private int level;
+	private double avg_consume;
+	private double sum_sale;
+	public int getShop_no() {
+		return shop_no;
+	}
+	public void setShop_no(int shop_no) {
+		this.shop_no = shop_no;
+	}
+	public String getShop_name() {
+		return shop_name;
+	}
+	public void setShop_name(String shop_name) {
+		this.shop_name = shop_name;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public double getAvg_consume() {
+		return avg_consume;
+	}
+	public void setAvg_consume(double avg_consume) {
+		this.avg_consume = avg_consume;
+	}
+	public double getSum_sale() {
+		return sum_sale;
+	}
+	public void setSum_sale(double sum_sale) {
+		this.sum_sale = sum_sale;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
