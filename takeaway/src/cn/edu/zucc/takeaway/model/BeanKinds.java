cfbcdb86a32360eca0cb1485c41f3212a684fdba
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:58:03 +0800

    Create BeanKinds.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanKinds.java
@@ -0,0 +1,98 @@
+package cn.edu.zucc.takeaway.model;
+
+import java.util.Date;
+
+public class BeanKinds {
+	
+	private int order_no;
+	private int shop_no;
+	private int kind_no;
+	private int rider_no;
+	private int count_no;
+	private int youhui_no;
+	private double money;
+	private double true_money;
+	private Date order_time;
+	private Date arrive;
+	private int site;
+	private int user_no;
+	
+
+	public int getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(int order_no) {
+		this.order_no = order_no;
+	}
+	public int getShop_no() {
+		return shop_no;
+	}
+	public void setShop_no(int shop_no) {
+		this.shop_no = shop_no;
+	}
+	public int getKind_no() {
+		return kind_no;
+	}
+	public void setKind_no(int kind_no) {
+		this.kind_no = kind_no;
+	}
+	public int getRider_no() {
+		return rider_no;
+	}
+	public void setRider_no(int rider_no) {
+		this.rider_no = rider_no;
+	}
+	public int getCount_no() {
+		return count_no;
+	}
+	public void setCount_no(int count_no) {
+		this.count_no = count_no;
+	}
+	public int getYouhui_no() {
+		return youhui_no;
+	}
+	public void setYouhui_no(int youhui_no) {
+		this.youhui_no = youhui_no;
+	}
+	public double getMoney() {
+		return money;
+	}
+	public void setMoney(double money) {
+		this.money = money;
+	}
+	public double getTrue_money() {
+		return true_money;
+	}
+	public void setTrue_money(double true_money) {
+		this.true_money = true_money;
+	}
+	public Date getOrder_time() {
+		return order_time;
+	}
+	public void setOrder_time(Date order_time) {
+		this.order_time = order_time;
+	}
+	public Date getArrive() {
+		return arrive;
+	}
+	public void setArrive(Date arrive) {
+		this.arrive = arrive;
+	}
+	public int getSite() {
+		return site;
+	}
+	public void setSite(int site) {
+		this.site = site;
+	}
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	
+	
+	
+	
+	
+}
