94c1cfe9c30a10eaa2bb3cae6f17886fee1a8732
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:58:12 +0800

    Create BeanRider.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanRider.java
@@ -0,0 +1,44 @@
+package cn.edu.zucc.takeaway.model;
+
+import java.util.Date;
+
+public class BeanRider {
+	private int rider_no;
+	private String rider_name;
+	private Date rider_start;
+	private int rider_level;
+	
+	
+	public int getRider_no() {
+		return rider_no;
+	}
+	public void setRider_no(int rider_no) {
+		this.rider_no = rider_no;
+	}
+	public String getRider_name() {
+		return rider_name;
+	}
+	public void setRider_name(String rider_name) {
+		this.rider_name = rider_name;
+	}
+	public Date getRider_start() {
+		return rider_start;
+	}
+	public void setRider_start(Date rider_start) {
+		this.rider_start = rider_start;
+	}
+	public int getRider_level() {
+		return rider_level;
+	}
+	public void setRider_level(int rider_level) {
+		this.rider_level = rider_level;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
