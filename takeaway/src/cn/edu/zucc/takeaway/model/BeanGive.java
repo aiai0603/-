b05f063bac950576888d0445dfd7c88a4b0cbeea
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:57:57 +0800

    Create BeanGive.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanGive.java
@@ -0,0 +1,43 @@
+package cn.edu.zucc.takeaway.model;
+
+public class BeanGive {
+	private int user_no;
+	private int shop_no;
+	private int need;//需要几单
+	private int already;//已经几单
+	private int youhui_no;
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public int getShop_no() {
+		return shop_no;
+	}
+	public void setShop_no(int shop_no) {
+		this.shop_no = shop_no;
+	}
+	public int getNeed() {
+		return need;
+	}
+	public void setNeed(int need) {
+		this.need = need;
+	}
+	public int getAlready() {
+		return already;
+	}
+	public void setAlready(int already) {
+		this.already = already;
+	}
+	public int getYouhui_no() {
+		return youhui_no;
+	}
+	public void setYouhui_no(int youhui_no) {
+		this.youhui_no = youhui_no;
+	}
+	
+	
+	
+	
+}
