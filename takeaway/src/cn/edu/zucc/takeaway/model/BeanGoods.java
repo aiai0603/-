f625f1a405f6abd5cee307cd82e108b134259e89
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:58:01 +0800

    Create BeanGoods.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanGoods.java
@@ -0,0 +1,48 @@
+package cn.edu.zucc.takeaway.model;
+
+public class BeanGoods {
+	
+	private int good_no;
+	private int kind_no;
+	private String good_name;
+	private double good_price;
+	private double good_sale;
+	public int getGood_no() {
+		return good_no;
+	}
+	public void setGood_no(int good_no) {
+		this.good_no = good_no;
+	}
+	public int getKind_no() {
+		return kind_no;
+	}
+	public void setKind_no(int kind_no) {
+		this.kind_no = kind_no;
+	}
+	public String getGood_name() {
+		return good_name;
+	}
+	public void setGood_name(String good_name) {
+		this.good_name = good_name;
+	}
+	public double getGood_price() {
+		return good_price;
+	}
+	public void setGood_price(double good_price) {
+		this.good_price = good_price;
+	}
+	public double getGood_sale() {
+		return good_sale;
+	}
+	public void setGood_sale(double good_sale) {
+		this.good_sale = good_sale;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
