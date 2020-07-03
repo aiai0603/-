cc6ff4268cd57edd13418a8989c7942a3dcde488
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:57:51 +0800

    Create BeanAdmin.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanAdmin.java
@@ -0,0 +1,29 @@
+package cn.edu.zucc.takeaway.model;
+
+public class BeanAdmin {
+	private int admin_no;
+	private String admin_name;
+	private String pwd;
+
+	public int getAdmin_no() {
+		return admin_no;
+	}
+	public void setAdmin_no(int admin_no) {
+		this.admin_no = admin_no;
+	}
+	public String getAdmin_name() {
+		return admin_name;
+	}
+	public void setAdmin_name(String admin_name) {
+		this.admin_name = admin_name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	
+}
