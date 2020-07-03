a62c6b8954047c35214d6acb59194b450118d0a7
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:57:48 +0800

    Create BeanAddresser.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanAddresser.java
@@ -0,0 +1,55 @@
+package cn.edu.zucc.takeaway.model;
+
+public class BeanAddresser {
+	private int address_no;
+	private int user_no;
+	private String sheng;
+	private String shi;
+	private String address;
+	private String call_user;
+	private String address_tele;
+	public int getAddress_no() {
+		return address_no;
+	}
+	public void setAddress_no(int address_no) {
+		this.address_no = address_no;
+	}
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public String getSheng() {
+		return sheng;
+	}
+	public void setSheng(String sheng) {
+		this.sheng = sheng;
+	}
+	public String getShi() {
+		return shi;
+	}
+	public void setShi(String shi) {
+		this.shi = shi;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCall_user() {
+		return call_user;
+	}
+	public void setCall_user(String call_user) {
+		this.call_user = call_user;
+	}
+	public String getAddress_tele() {
+		return address_tele;
+	}
+	public void setAddress_tele(String address_tele) {
+		this.address_tele = address_tele;
+	}
+	
+	
+}
