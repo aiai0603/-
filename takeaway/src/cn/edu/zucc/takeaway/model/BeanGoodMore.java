6d66aa35f619de14050e8bc8eeb01a44f63b997e
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-03 21:57:59 +0800

    Create BeanGoodMore.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanGoodMore.java
@@ -0,0 +1,45 @@
+package cn.edu.zucc.takeaway.model;
+
+public class BeanGoodMore {
+	private int order_no;
+	private int good_no;
+	private int good_count;
+	private double good_price;
+	private double good_sale;
+	public int getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(int order_no) {
+		this.order_no = order_no;
+	}
+	public int getGood_no() {
+		return good_no;
+	}
+	public void setGood_no(int good_no) {
+		this.good_no = good_no;
+	}
+	public int getGood_count() {
+		return good_count;
+	}
+	public void setGood_count(int good_count) {
+		this.good_count = good_count;
+	}
+	public double getGood_price() {
+		return good_price;
+	}
+	public void setGood_price(double good_price) {
+		this.good_price = good_price;
+	}
+	public double getGood_sale() {
+		return good_sale;
+	}
+	public void setGood_sale(double good_sale) {
+		this.good_sale = good_sale;
+	}
+	
+	
+	
+	
+	
+	
+}
