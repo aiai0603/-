4271c8647ab1b2dfc4377411162528155701dc65
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-04 13:37:16 +0800

    Create PersonPlanStarter.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/PersonPlanStarter.java
@@ -0,0 +1,15 @@
+package cn.edu.zucc.takeaway;
+
+import cn.edu.zucc.takeaway.ui.FrmMain;
+
+public class PersonPlanStarter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FrmMain();
+	}
+
+}
