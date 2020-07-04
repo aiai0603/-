ec1ad8dec2846b47b6903c598d5766a904e16e5f
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-04 13:37:23 +0800

    Create FrmLoginAdmin.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/ui/FrmLoginAdmin.java
@@ -0,0 +1,107 @@
+package cn.edu.zucc.takeaway.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import cn.edu.zucc.takeaway.control.ExampleUserManager;
+import cn.edu.zucc.takeaway.model.BeanAdmin;
+import cn.edu.zucc.takeaway.model.BeanUsers;
+import cn.edu.zucc.takeaway.util.BaseException;
+
+
+
+
+
+
+public class FrmLoginAdmin extends JDialog implements ActionListener {
+	private JPanel toolBar = new JPanel();
+	private JPanel workPane = new JPanel();
+	private JButton btnLogin = new JButton("登陆");
+	private JButton btnCancel = new JButton("退出");
+
+	
+	private JLabel labelUser = new JLabel("用户：");
+	private JLabel labelPwd = new JLabel("密码：");
+	private JTextField edtUserId = new JTextField(20);
+	private JPasswordField edtPwd = new JPasswordField(20);
+
+	public FrmLoginAdmin(FrmLoading frmLoading, String s, boolean b) {
+		super(frmLoading, s, b);
+		String sig=s;
+		toolBar.setLayout(new FlowLayout(FlowLayout.RIGHT));
+	
+		toolBar.add(btnLogin);
+		toolBar.add(btnCancel);
+		this.getContentPane().add(toolBar, BorderLayout.SOUTH);
+		workPane.add(labelUser);
+		workPane.add(edtUserId);
+		workPane.add(labelPwd);
+		workPane.add(edtPwd);
+		
+		this.getContentPane().add(workPane, BorderLayout.CENTER);
+		this.setSize(300, 140);
+		// 屏幕居中显示
+		double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		this.setLocation((int) (width - this.getWidth()) / 2,
+				(int) (height - this.getHeight()) / 2);
+
+		this.validate();
+
+		btnLogin.addActionListener(this);
+		btnCancel.addActionListener(this);
+	
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 自动生成的方法存根
+		if (e.getSource() == this.btnLogin) {
+			ExampleUserManager ex=new ExampleUserManager();
+			String username=this.edtUserId.getText();
+			String pwd=new String(this.edtPwd.getPassword());
+			
+			try {
+				BeanAdmin.currentLoginAdmin= ex.loginAdmin(username, pwd);
+				
+				if(pwd.equals(BeanAdmin.currentLoginAdmin.getPwd())){
+					
+					setVisible(false);
+				}
+				
+				
+				else{
+					JOptionPane.showMessageDialog(null,  "密码错误","错误提示",JOptionPane.ERROR_MESSAGE);
+				}
+			} catch (BaseException e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			
+		} else if (e.getSource() == this.btnCancel) {
+			this.setVisible(false);
+			FrmLoading.ok=1;
+		} 
+		
+	}
+
+	
+	
+}
