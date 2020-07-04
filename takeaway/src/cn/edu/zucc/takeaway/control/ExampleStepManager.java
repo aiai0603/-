aee2b4deac0ead45fa5e6c95f17e947c91e07c62
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-04 13:36:59 +0800

    Create ExampleStepManager.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/control/ExampleStepManager.java
@@ -0,0 +1,18 @@
+package cn.edu.zucc.takeaway.control;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+
+public class ExampleStepManager  {
+
+	
+
+}
