eb42e04cdc6ba1502ae8ae95160320bda76270c5
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-04 13:37:05 +0800

    Create BeanKind.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanKind.java
@@ -0,0 +1,43 @@
+package cn.edu.zucc.takeaway.model;
+
+public class BeanKind {
+	
+	
+	private int shop_no;
+	private int kind_no;
+	private String kind_name;
+	private int kind_sum;//商品数量
+	
+	public int getShop_no() {
+		return shop_no;
+	}
+	public void setShop_no(int shop_no) {
+		this.shop_no = shop_no;
+	}
+	public int getKind_no() {
+		return kind_no;
+	}
+	public void setKind_no(int kind_no) {
+		this.kind_no = kind_no;
+	}
+	public String getKind_name() {
+		return kind_name;
+	}
+	public void setKind_name(String kind_name) {
+		this.kind_name = kind_name;
+	}
+	public int getKind_sum() {
+		return kind_sum;
+	}
+	public void setKind_sum(int kind_sum) {
+		this.kind_sum = kind_sum;
+	}
+	
+
+	
+	
+	
+	
+	
+	
+}
