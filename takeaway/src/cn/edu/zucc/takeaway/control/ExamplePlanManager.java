5534ba0c2a24a4dcb6de4b35187edbacd25b6e58
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-04 13:36:57 +0800

    Create ExamplePlanManager.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/control/ExamplePlanManager.java
@@ -0,0 +1,16 @@
+package cn.edu.zucc.takeaway.control;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+
+
+public class ExamplePlanManager {
+
+	
+
+}
