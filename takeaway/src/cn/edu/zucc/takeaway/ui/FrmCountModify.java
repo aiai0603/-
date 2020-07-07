e5476330bdf702e297c31b7c634c99b16aabc3c1
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-07 22:22:39 +0800

    Create FrmCountModify.java

---
+++ takeaway/src/cn/edu/zucc/takeaway/ui/FrmCountModify.java
@@ -0,0 +1,87 @@
+package cn.edu.zucc.takeaway.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+import cn.edu.zucc.takeaway.control.*;
+import cn.edu.zucc.takeaway.model.BeanAddresser;
+import cn.edu.zucc.takeaway.model.BeanGoodMore;
+import cn.edu.zucc.takeaway.model.BeanGoods;
+import cn.edu.zucc.takeaway.model.BeanOwnerCount;
+import cn.edu.zucc.takeaway.util.BaseException;
+
+
+
+
+public class FrmCountModify extends JDialog implements ActionListener {
+	private JPanel toolBar = new JPanel();
+	private JPanel workPane = new JPanel();
+	private Button btnOk = new Button("购买");
+	private Button btnCancel = new Button("取消");
+	
+	private JLabel labelUser = new JLabel("数量：");
+	private JTextField jt=new JTextField(20);
+	private int id;
+	private BeanGoodMore bean;
+	
+	public FrmCountModify(FrmBuy frmBuy, String s, boolean b,int orderid, BeanGoodMore beanGoodMore) {
+		super(frmBuy, s, b);
+		toolBar.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		toolBar.add(this.btnOk);
+		toolBar.add(btnCancel);
+		this.getContentPane().add(toolBar, BorderLayout.SOUTH);
+		workPane.add(labelUser);
+		workPane.add(jt);
+		jt.setText(Integer.toString(beanGoodMore.getGood_count()));
+		this.getContentPane().add(workPane, BorderLayout.CENTER);
+		this.setSize(290, 100);
+		double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		this.setLocation((int) (width - this.getWidth()) / 2,
+				(int) (height - this.getHeight()) / 2);
+		this.btnCancel.addActionListener(this);
+		this.btnOk.addActionListener(this);
+		bean=beanGoodMore;
+		id=orderid;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==this.btnCancel)
+			this.setVisible(false);
+		else if(e.getSource()==this.btnOk){
+		
+			ExampleGoodMoreManager ex =new ExampleGoodMoreManager();
+			try {
+				int count=Integer.parseInt(this.jt.getText());
+				ex.deleteordermore(bean,count);
+			} catch (Exception e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+				return;
+			} 
+			JOptionPane.showMessageDialog(null, "修改成功", "成功",JOptionPane.INFORMATION_MESSAGE);
+			this.setVisible(false);
+		}
+			
+		
+	}
+
+
+}
