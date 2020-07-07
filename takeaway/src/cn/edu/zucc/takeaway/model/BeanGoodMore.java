6361ccc881d14bbf06eefbd7b645900a65ecc0d2
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-07 22:22:28 +0800

    Update BeanGoodMore.java

--- takeaway/src/cn/edu/zucc/takeaway/model/BeanGoodMore.java
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanGoodMore.java
@@ -1,11 +1,19 @@
 package cn.edu.zucc.takeaway.model;
 
 public class BeanGoodMore {
+	public static final  String[] tableTitles= {"商品","购买数量","原价","实际价格"};
 	private int order_no;
 	private int good_no;
 	private int good_count;
 	private double good_price;
 	private double good_sale;
+	public String getGood_name() {
+		return good_name;
+	}
+	public void setGood_name(String good_name) {
+		this.good_name = good_name;
+	}
+	private String good_name;
 	public int getOrder_no() {
 		return order_no;
 	}
@@ -37,6 +45,14 @@ public class BeanGoodMore {
 		this.good_sale = good_sale;
 	}
 	
+	public String getCell(int col){
+		
+		if(col==0) return this.getGood_name();
+		else if(col==1)  return Integer.toString(this.getGood_count());
+		else if(col==2) return Double.toString(this.getGood_price());
+		else if(col==3) return Double.toString(this.getGood_sale());
+		else return "";
+	}
 	
 	
 	
