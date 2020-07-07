dbc483d2aa8e44e21c91c77156a70b04a8412763
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-07 22:22:40 +0800

    Update FrmMain.java

--- takeaway/src/cn/edu/zucc/takeaway/ui/FrmMain.java
+++ takeaway/src/cn/edu/zucc/takeaway/ui/FrmMain.java
@@ -1,11 +1,14 @@
 package cn.edu.zucc.takeaway.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
@@ -13,6 +16,8 @@ import java.awt.event.WindowEvent;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -22,13 +27,20 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
+import cn.edu.zucc.takeaway.control.ExampleOrderManager;
+import cn.edu.zucc.takeaway.control.ExampleShopManager;
 import cn.edu.zucc.takeaway.control.ExampleUserManager;
 import cn.edu.zucc.takeaway.model.BeanAdmin;
+import cn.edu.zucc.takeaway.model.BeanCounts;
+import cn.edu.zucc.takeaway.model.BeanShops;
 import cn.edu.zucc.takeaway.model.BeanUsers;
 import cn.edu.zucc.takeaway.ui.FrmLogin;
 import cn.edu.zucc.takeaway.util.BaseException;
+import cn.edu.zucc.takeaway.util.BusinessException;
+import cn.edu.zucc.takeaway.util.DbException;
 
 
 
@@ -66,8 +78,44 @@ public class FrmMain extends JFrame implements ActionListener {
 	private JMenu menu_order=new JMenu("点餐管理");
 	private JMenuItem  menuItem_address=new JMenuItem("我的地址");
 	private JMenuItem  menuItem_order=new JMenuItem("我的订单");
+	private JPanel toolBar = new JPanel();
+	private JTextField edtKeyword = new JTextField(10);
+	private JButton btnSearch = new JButton("查询商家");
+	private JPanel toolBar2 = new JPanel();
+	private JButton btnGo = new JButton("进入商家");
 	
-
+	private Object tblShop[]=BeanShops.tableTitles2;
+	private Object tblShopData[][];
+	DefaultTableModel tabShopModel=new DefaultTableModel();
+	private JTable dataTableShop=new JTable(tabShopModel);
+	
+	private Object tblCount[]=BeanCounts.tableTitles;
+	private Object tblCountData[][];
+	DefaultTableModel tabCountModel=new DefaultTableModel();
+	private JTable dataTableCount=new JTable(tabCountModel);
+	
+	String sort[]= {"高星级优先","消费最低优先","消费最高优先","销量高优先"};
+	JComboBox<String> cb=new JComboBox<String>(sort);
+	
+	
+	List<BeanShops> allshops=null;
+	
+	private void reloadPlanTable(String name){
+		try {
+			allshops=(new ExampleShopManager()).loadshop(name);
+		} catch (BaseException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		tblShopData =  new Object[allshops.size()][BeanShops.tableTitles2.length];
+		for(int i=0;i<allshops.size();i++){
+			for(int j=0;j<BeanShops.tableTitles2.length;j++)
+				tblShopData[i][j]=allshops.get(i).getCell(j);
+		}
+		tabShopModel.setDataVector(tblShopData,tblShop);
+		this.dataTableShop.validate();
+		this.dataTableShop.repaint();
+	}
 	
 	public FrmMain(){
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
@@ -84,6 +132,90 @@ public class FrmMain extends JFrame implements ActionListener {
 	
 		if(uskind==1)
 		{
+			toolBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+			toolBar.add(edtKeyword);
+			toolBar.add(btnSearch);
+			toolBar.add(cb);
+			toolBar.add(btnGo);
+			
+			this.btnSearch.addActionListener(this);
+			this.btnGo.addActionListener(this);
+			
+			 cb.addItemListener(new ItemListener() {
+		            @Override
+		            public void itemStateChanged(ItemEvent e) {
+		                if (e.getStateChange() == ItemEvent.SELECTED) {
+		                   if(e.getItem().equals("高星级优先")) {
+		                		try {
+		                			allshops=(new ExampleShopManager()).loadshop(edtKeyword.getText());
+		                		} catch (BaseException e1) {
+		                			JOptionPane.showMessageDialog(null, e1.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+		                			return;
+		                		}
+		                		tblShopData =  new Object[allshops.size()][BeanShops.tableTitles2.length];
+		                		for(int i=0;i<allshops.size();i++){
+		                			for(int j=0;j<BeanShops.tableTitles2.length;j++)
+		                				tblShopData[i][j]=allshops.get(i).getCell(j);
+		                		}
+		                		tabShopModel.setDataVector(tblShopData,tblShop);
+		                		dataTableShop.validate();
+		                		dataTableShop.repaint();
+		                   }
+		                else if(e.getItem().equals("消费最低优先")){
+		                	try {
+	                			allshops=(new ExampleShopManager()).loadshopbyavg1(edtKeyword.getText());
+	                		} catch (BaseException e1) {
+	                			JOptionPane.showMessageDialog(null, e1.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+	                			return;
+	                		}
+	                		tblShopData =  new Object[allshops.size()][BeanShops.tableTitles2.length];
+	                		for(int i=0;i<allshops.size();i++){
+	                			for(int j=0;j<BeanShops.tableTitles2.length;j++)
+	                				tblShopData[i][j]=allshops.get(i).getCell(j);
+	                		}
+	                		tabShopModel.setDataVector(tblShopData,tblShop);
+	                		dataTableShop.validate();
+	                		dataTableShop.repaint();
+		                   
+		                }else if(e.getItem().equals("消费最高优先")){
+		                	try {
+	                			allshops=(new ExampleShopManager()).loadshopbyavg2(edtKeyword.getText());
+	                		} catch (BaseException e1) {
+	                			JOptionPane.showMessageDialog(null, e1.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+	                			return;
+	                		}
+	                		tblShopData =  new Object[allshops.size()][BeanShops.tableTitles2.length];
+	                		for(int i=0;i<allshops.size();i++){
+	                			for(int j=0;j<BeanShops.tableTitles2.length;j++)
+	                				tblShopData[i][j]=allshops.get(i).getCell(j);
+	                		}
+	                		tabShopModel.setDataVector(tblShopData,tblShop);
+	                		dataTableShop.validate();
+	                		dataTableShop.repaint();
+		                	
+		                   }else if(e.getItem().equals("销量高优先")){
+		                	   try {
+		                			allshops=(new ExampleShopManager()).loadshopbysum(edtKeyword.getText());
+		                		} catch (BaseException e1) {
+		                			JOptionPane.showMessageDialog(null, e1.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+		                			return;
+		                		}
+		                		tblShopData =  new Object[allshops.size()][BeanShops.tableTitles2.length];
+		                		for(int i=0;i<allshops.size();i++){
+		                			for(int j=0;j<BeanShops.tableTitles2.length;j++)
+		                				tblShopData[i][j]=allshops.get(i).getCell(j);
+		                		}
+		                		tabShopModel.setDataVector(tblShopData,tblShop);
+		                		dataTableShop.validate();
+		                		dataTableShop.repaint();
+		                }
+		            }
+
+		            }
+		        });
+			 
+			 
+			this.getContentPane().add(toolBar, BorderLayout.NORTH);
 			this.setTitle("用户系统");
 			this.menu_xx.add(this.menuItem_change); this.menuItem_change.addActionListener(this);
 		    this.menu_xx.add(this.menuItem_vip); this.menuItem_vip.addActionListener(this);
@@ -93,6 +225,13 @@ public class FrmMain extends JFrame implements ActionListener {
 			menubar2.add(menu_order);
 			this.setJMenuBar(menubar2);
 		    statusBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+		    JScrollPane js1=new JScrollPane(this.dataTableShop);
+		    js1.setPreferredSize(new Dimension(700, 10));
+		    JScrollPane js2=new JScrollPane(this.dataTableCount);
+		    js2.setPreferredSize(new Dimension(100, 10));
+		   this.getContentPane().add(js1, BorderLayout.WEST);
+		   this.getContentPane().add(js2, BorderLayout.CENTER);
+		 
 		    if(BeanUsers.currentLoginUser.isVip())
 		    {
 		    	SimpleDateFormat f=new SimpleDateFormat("yyyy-MM-dd");
@@ -111,6 +250,9 @@ public class FrmMain extends JFrame implements ActionListener {
 		    		System.exit(0);
 	             }
 	        });
+		    
+		    
+		    reloadPlanTable(edtKeyword.getText());
 		    this.setVisible(true);
 
 		}else if(uskind==2)
@@ -144,7 +286,7 @@ public class FrmMain extends JFrame implements ActionListener {
 		}
 				 	
 	}
-
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -188,9 +330,6 @@ public class FrmMain extends JFrame implements ActionListener {
 			    	label.setText("您好! 用户 "+BeanUsers.currentLoginUser.getUser_name()+"        开通会员，尽享优惠");
 					
 			    }
-		
-			
-			
 		}	else if(e.getSource()==this.menuItem_Deleteuser){
 			FrmDeleteUser dlg=new FrmDeleteUser (this,"用户管理",true);
 			dlg.setVisible(true);
@@ -208,6 +347,26 @@ public class FrmMain extends JFrame implements ActionListener {
 			dlg.setVisible(true);
 		} else if(e.getSource()==this.menuItem_address){
 			FrmAddress dlg=new FrmAddress(this,"地址管理",true);
+			dlg.setVisible(true);
+		}else if(e.getSource()==this.btnSearch) {
+			reloadPlanTable(edtKeyword.getText());
+			cb.setSelectedItem("高星级优先");
+		}else if(e.getSource()==this.btnGo) {
+			int i=this.dataTableShop.getSelectedRow();
+			if(i<0) {
+				JOptionPane.showMessageDialog(null, "请选择商家", "错误",JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			int id;
+			ExampleOrderManager ex=new ExampleOrderManager();
+			try {
+				 id=ex.addorder(BeanUsers.currentLoginUser.getUser_no(),allshops.get(i).getShop_no());
+			} catch (BaseException e1) {
+				// TODO 自动生成的 catch 块
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "错误",JOptionPane.ERROR_MESSAGE);
+				return;
+			} 
+			FrmBuy dlg=new FrmBuy(this,allshops.get(i).getShop_name()+"点餐",true,allshops.get(i),id);
 			dlg.setVisible(true);
 		}
 		
