b9d8db421bd229b27b7702b30a6cb71c61b951b1
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-08 22:08:17 +0800

    Update BeanCounts.java

--- takeaway/src/cn/edu/zucc/takeaway/model/BeanCounts.java
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanCounts.java
@@ -1,7 +1,7 @@
 package cn.edu.zucc.takeaway.model;
 
 public class BeanCounts {
-	public static final  String[] tableTitles= {"要求金额","减免金额","能否与优惠券叠加"};
+	public static final  String[] tableTitles= {"要求金额","减免金额"};
 	private int count_no;
 	private int shop_no;
 	private double ac_money;//满减金额
@@ -32,18 +32,12 @@ public class BeanCounts {
 	public void setCount_sale(double count_sale) {
 		this.count_sale = count_sale;
 	}
-	public boolean isTogether() {
-		return together;
-	}
-	public void setTogether(boolean together) {
-		this.together = together;
-	}
+	
 	
 	public String getCell(int col){
 		
 		if(col==0) return Double.toString(this.getAc_money());
 		else if(col==1) return Double.toString(this.getCount_sale());
-		else if(col==2) return Boolean.toString(this.isTogether());
 		else return "";
 	}
 	
