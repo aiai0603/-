42333846fc3645d686b02ee27f5d07b98802512c
aiai0603 <53207826+aiai0603@users.noreply.github.com>
2020-07-09 22:10:58 +0800

    Update BeanGive.java

--- takeaway/src/cn/edu/zucc/takeaway/model/BeanGive.java
+++ takeaway/src/cn/edu/zucc/takeaway/model/BeanGive.java
@@ -1,7 +1,30 @@
 package cn.edu.zucc.takeaway.model;
 
+import java.util.Date;
+
 public class BeanGive {
 	private int user_no;
+	private double youhui_sale;
+	private Date endtime;
+	public Date getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(Date endtime) {
+		this.endtime = endtime;
+	}
+	public double getYouhui_sale() {
+		return youhui_sale;
+	}
+	public void setYouhui_sale(double youhui_sale) {
+		this.youhui_sale = youhui_sale;
+	}
+	public String getShop_name() {
+		return shop_name;
+	}
+	public void setShop_name(String shop_name) {
+		this.shop_name = shop_name;
+	}
+	private String shop_name;
 	private int shop_no;
 	private int need;//需要几单
 	private int already;//已经几单
